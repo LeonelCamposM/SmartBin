372dead6c6ae81ffea3262c35684f4b1fe2483c4
LeonelCamposM <leonelscamposm@gmail.com>
2024-05-02 18:29:34 -0600

    update pin

--- Software/README.md
+++ Software/README.md
@@ -3,4 +3,4 @@
 // used wiring diagram
 https://wokwi.com/projects/360939448028148737
 
-changed servoPin to 1
+changed servoPin to 2 (D1 pin xiao-esp32S3)
--- Software/servoController/servoController.ino
+++ Software/servoController/servoController.ino
@@ -1,18 +1,19 @@
 #include <pwmWrite.h>
 
 Pwm pwm = Pwm();
-const int servoPin4 = 1;
+const int servoPin5 = 2; // D1 pin xiao-esp32S3
+const int freq = 50;
 
 void setup() {
   Serial.begin(115200);
-  pwm.attach(servoPin4, 4, 500, 1472, 2400);
+  pwm.attach(servoPin5, 5, 500, 1472, 2400);
   pwm.printDebug(); 
 }
 
 void loop() {
   int position;
   for (position = 0; position <= 180; position += 30) {
-    pwm.writeServo(servoPin4, position);
+    pwm.writeServo(servoPin5, 180 - position);
     delay(1000);
   }
 }
